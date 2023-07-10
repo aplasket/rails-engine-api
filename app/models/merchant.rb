691ea2087d383007dec3ac3ada48d3715689d97a
Ashley Plasket <ashley.plasket@gmail.com>
2023-07-10 17:56:44 -0400

    model associations

---
+++ app/models/merchant.rb
@@ -0,0 +1,6 @@
+class Merchant < ApplicationRecord
+  has_many :items
+  has_many :invoices
+  has_many :customers, through: :invoices
+  has_many :invoice_items, through: :items
+end
---
+++ spec/models/merchant_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe Merchant do
+  describe "relationships" do
+    it { should have_many :items }
+    it { should have_many :invoices }
+    it { should have_many(:customers).through(:invoices) }
+    it { should have_many(:invoice_items).through(:items) }
+  end
+end
