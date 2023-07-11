fa3d82a14960feaf59a3cd62d7623f86695a2555
Ashley Plasket <ashley.plasket@gmail.com>
2023-07-11 16:33:07 -0400

    fix items controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,22 +1,18 @@
-module Api
-  module V1
-    class ItemsController < ApplicationController
-      def index
-        render json: ItemSerializer.new(Item.all)
-      end
+class Api::V1::ItemsController < ApplicationController
+  def index
+    render json: ItemSerializer.new(Item.all)
+  end
 
-      def show
-        render json: ItemSerializer.new(Item.find(params[:id]))
-      end
+  def show
+    render json: ItemSerializer.new(Item.find(params[:id]))
+  end
 
-      def create
-        render json: ItemSerializer.new(Item.create!(item_params)), status: 201
-      end
+  def create
+    render json: ItemSerializer.new(Item.create!(item_params)), status: 201
+  end
 
-      private
-      def item_params
-        params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
-      end
-    end
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
   end
 end
