95c33e70c8237baeca98a4b35743cbb680230eca
Ashley Plasket <ashley.plasket@gmail.com>
2023-07-11 16:29:31 -0400

    WIP troubleshooting

---
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::Merchants::ItemsController < ApplicationController
+  # before_action :find_merchant, only: [:index]
+
+  def index
+    merchant = Merchant.find(params[:merchant_id])
+    merchant_items = merchant.items
+    render json: ItemSerializer.new(merchant_items)
+  end
+  # merchant_items = @merchant.items
+
+  private
+  # def find_merchant
+  #   @merchant = Merchant.find(params[:merchant_id].to_i)
+  #   # if @merchant.nil?
+  #   #   message = "No Merchant with merchant_id=#{params[:merchant_id].to_i}) exists"
+  #   #   raise ActiveRecord::RecordNotFound, message
+  #   # end
+  # end
+end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -52,6 +52,47 @@ RSpec.describe "Merchant Endpoints" do
   end
 
   describe "get all items for a given merchant id" do
+    it "lists out all items for a merchant" do
+      merchant1 = create(:merchant)
 
+      item1 = create(:item, merchant_id: merchant1.id)
+      item2 = create(:item, merchant_id: merchant1.id)
+      item3 = create(:item, merchant_id: merchant1.id)
+
+      get "/api/v1/merchants/#{merchant1.id}/items"
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      merchant1_items = JSON.parse(response.body, symbolize_names: true)
+      expect(merchant1_items).to have_key(:data)
+      expect(merchant1_items[:data]).to be_an(Array)
+      expect(merchant1_items[:data].count).to eq(3)
+
+      item = merchant1_items[:data][0]
+
+      expect(item).to be_a(Hash)
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_a(String)
+      expect(item[:id]).to eq(item1.id.to_s)
+
+      expect(item).to have_key(:attributes)
+      expect(item[:attributes][:name]).to be_a(String)
+      expect(item[:attributes][:name]).to eq(item1.name)
+      expect(item[:attributes][:description]).to be_a(String)
+      expect(item[:attributes][:description]).to eq(item1.description)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+      expect(item[:attributes][:unit_price]).to eq(item1.unit_price)
+      expect(item[:attributes][:merchant_id]).to be_an(Integer)
+      expect(item[:attributes][:merchant_id]).to eq(merchant1.id)
+    end
+
+    xit "lists error code if merchant can't be found" do
+
+      get "/api/v1/merchants/101/items"
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+    end
   end
 end
