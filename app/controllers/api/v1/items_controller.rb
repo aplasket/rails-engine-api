6fb4062f7d917fd4bfe00ccc7ef60860b73f244a
Ashley Plasket <ashley.plasket@gmail.com>
2023-07-11 13:00:02 -0400

    add tests route and action for create new item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,9 +1,22 @@
-class Api::V1::ItemsController < ApplicationController
-  def index
-    render json: ItemSerializer.new(Item.all)
-  end
+module Api
+  module V1
+    class ItemsController < ApplicationController
+      def index
+        render json: ItemSerializer.new(Item.all)
+      end
 
-  def show
-    render json: ItemSerializer.new(Item.find(params[:id]))
+      def show
+        render json: ItemSerializer.new(Item.find(params[:id]))
+      end
+
+      def create
+        render json: ItemSerializer.new(Item.create!(item_params)), status: 201
+      end
+
+      private
+      def item_params
+        params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+      end
+    end
   end
 end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -80,4 +80,29 @@ RSpec.describe "Items Endpoints" do
       expect(item_info[:data][:attributes][:merchant_id]).to_not eq(item3.merchant_id)
     end
   end
+
+  describe "creates one item" do
+    it "creates a new item" do
+      item_params = ({
+        name: "Widget",
+        description: "High quality widget",
+        unit_price: 100.99,
+        merchant_id: create(:merchant).id
+      })
+
+      headers = { "CONTENT_TYPE" => "application/json "}
+
+      post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
+
+      created_item = Item.last
+
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+
+      expect(created_item.name).to eq(item_params[:name])
+      expect(created_item.description).to eq(item_params[:description])
+      expect(created_item.unit_price).to eq(item_params[:unit_price])
+      expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+    end
+  end
 end
