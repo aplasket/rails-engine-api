609783dff061683012d22b984ff0778b4ddb6fb2
Ashley Plasket <ashley.plasket@gmail.com>
2023-07-12 18:05:43 -0400

    add model test and method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,8 @@ class Merchant < ApplicationRecord
   has_many :invoices
   has_many :customers, through: :invoices
   has_many :invoice_items, through: :items
+
+  def self.find_by_name(fragment)
+    where("name ILIKE '%#{fragment}%'").order(:name)
+  end
 end
