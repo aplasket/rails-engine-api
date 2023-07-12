aaab6e99bd2083af30d4c8ce357d8aa05918861e
Ashley Plasket <ashley.plasket@gmail.com>
2023-07-12 18:53:30 -0400

    add item model test to fetch item by name

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  def self.find_all_by_name(fragment)
+    where("name ILIKE '%#{fragment}%'")
+  end
 end
