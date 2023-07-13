88a129867616861afadc6eee05d67556cdcc9c9c
Ashley Plasket <ashley.plasket@gmail.com>
2023-07-12 20:16:03 -0400

    remove notes

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -2,12 +2,5 @@ class Api::V1::Items::SearchController < ApplicationController
   def index
     items = Item.find_all(params)
     render json: ItemSerializer.new(items)
-
-    # if params[:name]
-    #   render json: ItemSerializer.new(Item.find_all_by_name(params[:name]))
-    # elsif params[:min_price] || params[:max_price]
-    #   binding.pry
-    #   render json: ItemSerializer.new(Item.find_by_price(params))
-    # end
   end
 end
