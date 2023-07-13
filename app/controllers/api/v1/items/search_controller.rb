eb0ea92de047e32ed7783113f63a27405feaf117
Ashley Plasket <ashley.plasket@gmail.com>
2023-07-12 20:15:41 -0400

    add controller action

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,10 +1,13 @@
 class Api::V1::Items::SearchController < ApplicationController
   def index
-    if params[:name]
-      render json: ItemSerializer.new(Item.find_all_by_name(params[:name]))
+    items = Item.find_all(params)
+    render json: ItemSerializer.new(items)
+
+    # if params[:name]
+    #   render json: ItemSerializer.new(Item.find_all_by_name(params[:name]))
     # elsif params[:min_price] || params[:max_price]
     #   binding.pry
     #   render json: ItemSerializer.new(Item.find_by_price(params))
-    end
+    # end
   end
 end
