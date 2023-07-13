b13d29d3d2ffea492f10923f4d139ea7de08127e
Ashley Plasket <ashley.plasket@gmail.com>
2023-07-12 20:49:40 -0400

    add conditional

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::Items::SearchController < ApplicationController
   def index
     items = Item.find_all(params)
-    render json: ItemSerializer.new(items)
+  
+    if items[0].class == Item
+      render json: ItemSerializer.new(items)
+    else
+      render json: { error: 'Bad Request' }, status: 404
+    end
   end
 end
