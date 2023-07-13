627585384d1a695b4868f7a4d5b2692bc24caefd
Ashley Plasket <ashley.plasket@gmail.com>
2023-07-12 20:15:50 -0400

    add model tests and methods

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,26 @@ class Item < ApplicationRecord
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
 
+  def self.find_all(params)
+    if params[:name] && (params[:min_price] || params[:max_price])
+      "error - name cannot be searched with price queries"
+    elsif params[:min_price] || params[:max_price]
+      Item.find_by_price(params)
+    elsif !params[:name].empty?
+      Item.find_all_by_name(params[:name])
+    else
+      "error - query is invalid, parameter cannot be empty"
+    end
+  end
+
   def self.find_all_by_name(fragment)
     where("name ILIKE '%#{fragment}%'")
+  end
+
+  def self.find_by_price(params)
+    @min_price = params[:min_price].present? ? params[:min_price] : 0
+    @max_price = params[:max_price].present? ? params[:max_price] : 999_999
+
+    Item.where("unit_price >= #{@min_price} AND unit_price <= #{@max_price}")
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -8,20 +8,44 @@ RSpec.describe Item do
   end
 
   describe "#class methods" do
+    before(:each) do
+      @merchant1 = create(:merchant)
+      @merchant2 = create(:merchant)
+      @merchant3 = create(:merchant)
+
+      @item1 = create(:item, merchant_id: @merchant1.id, name: "Acyrlic paint", unit_price: 3.00 )
+      @item2 = create(:item, merchant_id: @merchant1.id, name: "Watercolor paint", unit_price: 7.00 )
+      @item3 = create(:item, merchant_id: @merchant2.id, name: "Paintbrush", unit_price: 13.00 )
+      @item4 = create(:item, merchant_id: @merchant2.id, name: "Paintball Gun", unit_price: 123.00 )
+      @item5 = create(:item, merchant_id: @merchant3.id, name: "lavious painting", unit_price: 4.99 )
+      @item6 = create(:item, merchant_id: @merchant3.id, name: "Sandpainting", unit_price: 99.99 )
+    end
+
+    it "#finds_all" do
+      expect(Item.find_all(name: "paint").count).to eq(6)
+      expect(Item.find_all(name: "paint", min_price: 4.99)).to eq("error - name cannot be searched with price queries")
+      expect(Item.find_all(name: "paint", min_price: 4.99, max_price: 99.99)).to eq("error - name cannot be searched with price queries")
+      expect(Item.find_all(min_price: 4.99).count).to eq(5)
+      expect(Item.find_all(max_price: 99.99).count).to eq(5)
+      expect(Item.find_all(min_price: 4.99, max_price: 99.99).count).to eq(4)
+      expect(Item.find_all(name: "")).to eq("error - query is invalid, parameter cannot be empty")
+    end
+
     it "#find_by_name" do
-      merchant1 = create(:merchant)
-      merchant2 = create(:merchant)
-      merchant3 = create(:merchant)
-
-      item1 = create(:item, merchant_id: merchant1.id, name: "acyrlic paint", unit_price: 3.00 )
-      item2 = create(:item, merchant_id: merchant1.id, name: "watercolor paint", unit_price: 5.00 )
-      item3 = create(:item, merchant_id: merchant2.id, name: "paintbrush", unit_price: 13.00 )
-      item4 = create(:item, merchant_id: merchant2.id, name: "paintball gun", unit_price: 23.00 )
-      item5 = create(:item, merchant_id: merchant3.id, name: "lavious painting", unit_price: 7.00 )
-      item6 = create(:item, merchant_id: merchant3.id, name: "sandpainting", unit_price: 103.00 )
-
       expect(Item.find_all_by_name("paint").count).to eq(6)
-      expect(Item.find_all_by_name("paint")[0]).to eq(item1)
+      expect(Item.find_all_by_name("paint")[0]).to eq(@item1)
+    end
+
+    it "#find by price" do
+      expect(Item.find_by_price(min_price: 4.99)).to eq([@item2, @item3, @item4, @item5, @item6])
+      expect(Item.find_by_price(min_price: 4.99)).to_not eq([@item1, @item2, @item3, @item4, @item5, @item6])
+      expect(Item.find_by_price(min_price: 4.99).count).to eq(5)
+
+      expect(Item.find_by_price(max_price: 99.99).count).to eq(5)
+      expect(Item.find_by_price(max_price: 99.99)).to eq([@item1, @item2, @item3, @item5, @item6])
+
+      expect(Item.find_by_price(min_price: 4.99, max_price: 99.99).count).to eq(4)
+      expect(Item.find_by_price(min_price: 4.99, max_price: 99.99)).to eq([@item2, @item3, @item5, @item6])
     end
   end
 end
